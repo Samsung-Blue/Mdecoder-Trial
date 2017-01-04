43cb927280f7aa7d3fba1cd9e21148efbdd21173
Samsung-Blue <sreelakshmias97@gmail.com>
2017-01-04 22:09:41 +0530

    Make changes to theme 1

--- animate4.js
+++ animate4.js
@@ -11,18 +11,18 @@ function addDigits()
 	for(i=0;i<=14;i++)
 	{
 		digits[i]={};
-		digits[i].digit=Math.floor(Math.random()*9);
-		digits[i].speedX = Math.floor(5*Math.random())+1;
-		digits[i].speedY = Math.floor(5*Math.random())+1;
+		digits[i].digit=Math.floor(Math.random()*10);
+		digits[i].speedX = Math.floor(10*Math.random())-5;
+		digits[i].speedY = Math.floor(10*Math.random())-5;
 		digits[i].posX = Math.floor(canvas.width*Math.random());
 		digits[i].posY = Math.floor(canvas.height*Math.random());
 		//Font Size from 30px to 230px
 		digits[i].fontSize = Math.floor(200*Math.random())+30;
 		digits[i].fontSpeed = Math.floor(4*Math.random())+1;
- 		ctx.font = digits[i].fontSize+"px Arial";
- 		rc = Math.floor(255*Math.random());
-		gc = Math.floor(255*Math.random());
-		bc = Math.floor(255*Math.random());
+ 		ctx.font = digits[i].fontSize+"px Gloria Hallelujah";
+ 		rc = Math.floor(256*Math.random());
+		gc = Math.floor(256*Math.random());
+		bc = Math.floor(256*Math.random());
 		digits[i].rc = rc;
 		digits[i].gc = gc;
 		digits[i].bc = bc;
@@ -55,7 +55,7 @@ function animate()
 		}
 		if(digits[i].fontSize>=230||digits[i].fontSize<=30)
 			digits[i].fontSpeed=-digits[i].fontSpeed		
-		ctx.font = digits[i].fontSize+"px Arial";
+		ctx.font = digits[i].fontSize+"px Gloria Hallelujah";
 		ctx.fillStyle = "rgb("+digits[i].rc+","+digits[i].gc+','+digits[i].bc+")";
 		ctx.fillText(digits[i].digit,digits[i].posX,digits[i].posY);
 	} 
